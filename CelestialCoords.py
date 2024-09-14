fix sign of y axis

CelestialCoords.py
@@ -76,7 +76,7 @@ z_axis_u, z_axis_v, z_axis_w = (0, np.sin(tilt), np.cos(tilt))
 # Define y-axis, which complete the 
 # right-hand triad (correct name in English?)
 y_axis_x, y_axis_y, y_axis_z = (0, 0, 0)
-y_axis_u, y_axis_v, y_axis_w = np.cross([x_axis_u, x_axis_v, x_axis_w], [z_axis_u, z_axis_v, z_axis_w])
+y_axis_u, y_axis_v, y_axis_w = -np.cross([x_axis_u, x_axis_v, x_axis_w], [z_axis_u, z_axis_v, z_axis_w])
 
 # Plot GCRS axes
 ax.quiver(x_axis_x, x_axis_y, x_axis_z, x_axis_u, x_axis_v, x_axis_w, color='cyan', length = 2.5, arrow_length_ratio=0.1, label='GCRS x-axis')